31833dad4ca77d4c507281b71922cd22018ff380
ayan-IN <ayanchakraborty.inbox@gmail.com>
2023-04-29 00:18:38 +0530

    BSA: Adding Bubble Sort :zap:

---
+++ 02-Basic_Sorting_Algorithms/01-Bubble_Sort.cpp
@@ -0,0 +1,46 @@
+//* @Desc - Bubble Sort -> Take larger element to the end by repeatedly swapping the adjacent elements.
+//? @Time Complexity -> O(n^2) [Worst Case Time Complexity]
+//? @Space Complexity -> O(1)
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int times = 1; times <= n - 1; times++)
+    {
+        //* Repeated Swapping
+        for (int j = 0; j <= n - times - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+void printArray(int arr[], int n)
+{
+    cout << "Array Contents" << endl;
+    cout << "----------------" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+            cout << arr[i] << endl;
+        else
+        {
+            cout << arr[i] << ", ";
+        }
+    }
+}
+
+int main()
+{
+    system("clear");
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    int n = sizeof(arr) / sizeof(int);
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
