c15be429866adae669f4f65be78fd616bad51e8c
ayan-IN <ayanchakraborty.inbox@gmail.com>
2023-05-10 16:50:23 +0530

    Character Arrays Problem : cin.getline usecase :tada:

---
+++ 03-Character_Arrays(Strings)/03-getLine.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("clear");
+    char sentence[1000];
+    cin.getline(sentence, 1000, '\n');
+    cout << sentence << endl;
+    return 0;
+}
